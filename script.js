08b3669d9e9d576087050a37f9ec2bec243d9907
Macusi Jhorex <macusijhorex25@gmail.com>
2025-10-29 07:00:37 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+fetch('students.json')
+  .then(response => response.json())
+  .then(students => {
+    const container = document.getElementById('portfolio-list');
+    students.forEach(student => {
+      const card = document.createElement('div');
+      card.className = 'student-card';
+      card.innerHTML = `
+        <img src="${student.photo}" alt="${student.name}">
+        <h3>${student.name}</h3>
+        <p>${student.course} - Batch ${student.yearlevel-section}</p>
+        <p>${student.bio}</p>
+        <a href="${student.github}" target="_blank">GitHub Profile</a>
+      `;
+      container.appendChild(card);
+    });
+  });
